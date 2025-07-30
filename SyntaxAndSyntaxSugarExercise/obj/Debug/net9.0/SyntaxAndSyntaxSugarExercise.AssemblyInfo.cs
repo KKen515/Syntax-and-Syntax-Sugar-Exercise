9fe5d2156eb530d91d95cbc76a3c3b9da3653538
Kenton Knelle <kken51915@gmail.com>
2025-07-30 08:21:10 -0400

    Updated String Interpolation inside the Ternary Operator

--- SyntaxAndSyntaxSugarExercise/obj/Debug/net9.0/SyntaxAndSyntaxSugarExercise.AssemblyInfo.cs
+++ SyntaxAndSyntaxSugarExercise/obj/Debug/net9.0/SyntaxAndSyntaxSugarExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SyntaxAndSyntaxSugarExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f9040d0dd3419719e3092505bdac0e545f3f010")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+182bff1082f9e404869048e2bced9f436039f2da")]
 [assembly: System.Reflection.AssemblyProductAttribute("SyntaxAndSyntaxSugarExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SyntaxAndSyntaxSugarExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
